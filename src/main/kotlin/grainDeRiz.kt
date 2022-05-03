dffef944c090ebcfe84425b69814a12283c15986
qrouet <quentinrouet@hotmail.fr>
2022-05-03 13:58:21 +0200

    grains 2 riz

--- src/main/kotlin/grainDeRiz.kt
+++ src/main/kotlin/grainDeRiz.kt
@@ -1,3 +1,15 @@
+import java.math.BigInteger
+
 fun main(){
-    println("sdg")
+    var nbGrzRizCase = BigInteger.ONE
+    var nbGrzRizTotaux = BigInteger.ONE
+    println("Calcul du nombre de grains de riz par case")
+    println("Case 1 : $nbGrzRizCase")
+    for (case in 2..64){
+        nbGrzRizCase = nbGrzRizCase.multiply(BigInteger("2"))
+        println("Case $case : $nbGrzRizCase")
+        nbGrzRizTotaux = nbGrzRizTotaux.plus(nbGrzRizCase)
+    }
+    println("Grains Totaux : $nbGrzRizTotaux")
+
 }
